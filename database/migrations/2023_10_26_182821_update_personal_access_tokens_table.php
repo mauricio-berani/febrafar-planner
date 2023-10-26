ec0b71cd031c1f9a433f0c76045a5fccff547af2
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 15:29:38 -0300

    refactor: table personal_access_tokens changed

---
+++ database/migrations/2023_10_26_182821_update_personal_access_tokens_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdatePersonalAccessTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dropColumn('tokenable_id');
+            $table->dropColumn('tokenable_type');
+        });
+
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->uuid('tokenable_uuid');
+            $table->string('tokenable_type');
+            $table->index(['tokenable_uuid', 'tokenable_type'], 'tokenable_index');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->dropIndex('tokenable_index');
+            $table->dropColumn('tokenable_uuid');
+            $table->dropColumn('tokenable_type');
+        });
+
+        Schema::table('personal_access_tokens', function (Blueprint $table) {
+            $table->morphs('tokenable');
+        });
+    }
+}

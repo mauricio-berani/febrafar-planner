d5cc81608af17027245809b6a4e358e7f3b4dab4
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 16:57:02 -0300

    feat: resource for paginated responses created

---
+++ app/Traits/PaginationResource.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+trait PaginationResource
+{
+    /**
+     * Get pagination details.
+     *
+     * @return array<string, mixed>
+     */
+    public function paginationDetails()
+    {
+        return [
+            'first_page_url' => $this->resource->url(1),
+            'from'           => $this->resource->firstItem(),
+            'last_page'      => $this->resource->lastPage(),
+            'last_page_url'  => $this->resource->url($this->resource->lastPage()),
+            'links'          => $this->resource->links(),
+            'next_page_url'  => $this->resource->nextPageUrl(),
+            'path'           => $this->resource->resolveCurrentPath(),
+            'per_page'       => $this->resource->perPage(),
+            'prev_page_url'  => $this->resource->previousPageUrl(),
+            'to'             => $this->resource->lastItem(),
+            'total'          => $this->resource->total(),
+            'current_page'   => $this->resource->currentPage(),
+        ];
+    }
+}

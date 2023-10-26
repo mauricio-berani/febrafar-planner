de41b667c92a8c13d4857071b9befa9b4e8d68e0
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 16:41:57 -0300

    feat: login request created

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+/**
+ * Class LoginRequest
+ *
+ * @package App\Http\Requests\Auth
+ *
+ * This class represents a form request for user login.
+ */
+class LoginRequest extends FormRequest
+{
+
+    /**
+     * @var bool Stop validation on the first failure.
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool Always returns true.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email'    => 'required|email|exists:users,email',
+            'password' => 'required',
+        ];
+    }
+
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator The validator instance.
+     *
+     * @throws HttpResponseException Throws a HTTP response exception with a JSON payload containing the validation errors.
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json($validator->errors(), 422));
+    }
+
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator The validator instance.
+     *
+     * @throws HttpResponseException Throws a HTTP response exception with a JSON payload containing the validation errors.
+     */
+    public function messages(): array
+    {
+        return [
+            'email.required'    => 'Enter your email.',
+            'email.email'       => 'Enter a valid email.',
+            'email.exists'      => 'The provided credentials are incorrect.',
+            'password.required' => 'Enter your password.',
+        ];
+    }
+}

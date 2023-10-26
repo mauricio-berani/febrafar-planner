53b092e27e43b97b8e2c403da77642b1ad41e675
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 11:30:56 -0300

    feat: migration for task_types table created

--- database/migrations/2023_10_26_135446_create_task_types_table.php
+++ database/migrations/2023_10_26_135446_create_task_types_table.php
@@ -12,8 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('task_types', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+            $table->uuid('id')->primary();
+            $table->string('name');
+            $table->enum('status', ['visible', 'hidden'])->default('visible');
         });
     }
 

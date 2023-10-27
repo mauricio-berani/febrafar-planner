f28ed47b4e9f19b93a16ed040d65184d1b6560d3
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 21:49:51 -0300

    feat: api police created

---
+++ app/Policies/ApiPolicy.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\User;
+
+/**
+ * This class represents a base policy from which other policy classes can extend.
+ */
+class ApiPolicy
+{
+
+    /**
+     * The authenticated user.
+     *
+     * @var User
+     */
+    protected $user;
+
+    /**
+     * Create a new policy instance.
+     */
+    public function __construct()
+    {
+        $this->user = User::find(auth()->user()->id);
+    }
+}

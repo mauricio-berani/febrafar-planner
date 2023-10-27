d68b1628009fd0af6b266dfe5d66685f6bcf3892
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-26 21:38:55 -0300

    feat: user routes created

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\Auth\{LoginController, LogoutController, RegisterController};
+use App\Http\Controllers\Api\User\{
+    FindAllController as UserFindAllController,
+    FindAllMatchesController as UserFindAllMatchesController,
+    FindOneController as UserFindOneController,
+    CreateController as UserCreateController,
+    UpdateController as UserUpdateController,
+    DeleteController as UserDeleteController
+};
 
 /*
 |--------------------------------------------------------------------------
@@ -23,4 +31,13 @@ Route::prefix('auth')->name('auth.')->group(function () {
     Route::post('/login', [LoginController::class, 'login']);
     Route::post('/register', [RegisterController::class, 'register']);
     Route::middleware('auth:sanctum')->post('/logout', [LogoutController::class, 'logout']);
+});
+
+Route::middleware(['auth:sanctum'])->prefix('users')->name('users.')->group(function () {
+    Route::get('/match', [UserFindAllMatchesController::class, 'findAllMatches']);
+    Route::get('/', [UserFindAllController::class, 'findAll']);
+    Route::get('/{user}', [UserFindOneController::class, 'findOne']);
+    Route::post('/', [UserCreateController::class, 'create']);
+    Route::put('/{user}', [UserUpdateController::class, 'update']);
+    Route::delete('/{user}', [UserDeleteController::class, 'delete']);
 });

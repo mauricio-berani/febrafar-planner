a6cf52322bd0bdcafc9908f138b3f8c6f42325d1
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-27 12:41:07 -0300

    feat: task relationship function created

--- app/Models/User.php
+++ app/Models/User.php
@@ -53,4 +53,14 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    /**
+     * Get the tasks for the user.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function tasks()
+    {
+        return $this->hasMany(Task::class);
+    }
 }

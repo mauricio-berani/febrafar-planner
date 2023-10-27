378972ac50e85ef890a5de2220f72bcdbe659a5f
Maurício Vieira Silva <mauricio@berani.com.br>
2023-10-27 09:55:55 -0300

    refactor: routes changed

--- routes/api.php
+++ routes/api.php
@@ -2,23 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\Auth\{LoginController, LogoutController, RegisterController};
-use App\Http\Controllers\Api\User\{
-    FindAllController as UserFindAllController,
-    FindAllMatchesController as UserFindAllMatchesController,
-    FindOneController as UserFindOneController,
-    CreateController as UserCreateController,
-    UpdateController as UserUpdateController,
-    DeleteController as UserDeleteController
-};
-use App\Http\Controllers\Api\TaskCategory\{
-    FindAllController as TaskCategoryFindAllController,
-    FindAllMatchesController as TaskCategoryFindAllMatchesController,
-    FindOneController as TaskCategoryFindOneController,
-    CreateController as TaskCategoryCreateController,
-    UpdateController as TaskCategoryUpdateController,
-    DeleteController as TaskCategoryDeleteController
-};
+use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\Task\{TypeController, TaskController};
 
 /*
 |--------------------------------------------------------------------------
@@ -35,26 +21,35 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::prefix('auth')->name('auth.')->group(function () {
-    Route::post('/login', [LoginController::class, 'login']);
-    Route::post('/register', [RegisterController::class, 'register']);
-    Route::middleware('auth:sanctum')->post('/logout', [LogoutController::class, 'logout']);
+Route::prefix('auth')->name('auth.')->controller(AuthController::class)->group(function () {
+    Route::post('/login', 'login');
+    Route::post('/register', 'register');
+    Route::middleware('auth:sanctum')->post('/logout', 'logout');
 });
 
-Route::middleware(['auth:sanctum'])->prefix('users')->name('users.')->group(function () {
-    Route::get('/match', [UserFindAllMatchesController::class, 'findAllMatches']);
-    Route::get('/', [UserFindAllController::class, 'findAll']);
-    Route::get('/{user}', [UserFindOneController::class, 'findOne']);
-    Route::post('/', [UserCreateController::class, 'create']);
-    Route::put('/{user}', [UserUpdateController::class, 'update']);
-    Route::delete('/{user}', [UserDeleteController::class, 'delete']);
+Route::middleware(['auth:sanctum'])->prefix('users')->name('users.')->controller(UserController::class)->group(function () {
+    Route::get('/match', 'findAllMatches');
+    Route::get('/', 'findAll');
+    Route::get('/{user}', 'findOne');
+    Route::post('/', 'create');
+    Route::put('/{user}', 'update');
+    Route::delete('/{user}', 'delete');
 });
 
-Route::middleware(['auth:sanctum'])->prefix('task_categories')->name('task_categories.')->group(function () {
-    Route::get('/match', [TaskCategoryFindAllController::class, 'findAllMatches']);
-    Route::get('/', [TaskCategoryFindAllMatchesController::class, 'findAll']);
-    Route::get('/{user}', [TaskCategoryFindOneController::class, 'findOne']);
-    Route::post('/', [TaskCategoryCreateController::class, 'create']);
-    Route::put('/{user}', [TaskCategoryUpdateController::class, 'update']);
-    Route::delete('/{user}', [TaskCategoryDeleteController::class, 'delete']);
+Route::middleware(['auth:sanctum'])->prefix('task_types')->name('task_types.')->controller(TypeController::class)->group(function () {
+    Route::get('/match', 'findAllMatches');
+    Route::get('/', 'findAll');
+    Route::get('/{user}', 'findOne');
+    Route::post('/', 'create');
+    Route::put('/{user}', 'update');
+    Route::delete('/{user}', 'delete');
+});
+
+Route::middleware(['auth:sanctum'])->prefix('tasks')->name('tasks.')->controller(TaskController::class)->group(function () {
+    Route::get('/match', 'findAllMatches');
+    Route::get('/', 'findAll');
+    Route::get('/{user}', 'findOne');
+    Route::post('/', 'create');
+    Route::put('/{user}', 'update');
+    Route::delete('/{user}', 'delete');
 });
